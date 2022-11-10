700a4e4c7bd9ec9e32fe0b2dde9be2010947f8aa
Nina Gerszberg <ninager@mit.edu>
2022-11-10 18:56:19 -0500

    serializing messages successfully

--- DNF_board.py
+++ DNF_board.py
@@ -1,8 +1,60 @@
+from hashlib import new
 import serial
 from serial.tools import list_ports
 import struct
 
 PORT_NAME = None
+msg_keys = {'start_board' : '111' , 'end_board': '000' , 'start_line' : '110', 'end_line': '001', 'and':'101', 'or':'010'}
+
+
+#ser = serial.Serial('/dev/cu.usbserial-AQ00J7ZN')
+
+def convert_to_binary_string(number):
+    """
+    takes an integer and returns a string
+    representing this integer in 12 bits
+    """
+    ret = format(number, '12b')
+    full=''
+    for i in ret:
+        if i=='1':
+            full+='1'
+        else:
+            full+='0'
+    return full
+
+#print(convert_to_binary_string(3))
+
+def make_serial(solutions):
+    
+    generic_end = '0'*13
+    start_board = msg_keys['start_board'] +generic_end
+    end_board = msg_keys['end_board'] + generic_end
+    new_line = msg_keys['start_line'] + generic_end
+    end_line = msg_keys['end_line'] + generic_end
+    new_or = msg_keys['or'] + generic_end
+    
+    ans = start_board
+    
+    for line in solutions:
+        ans += new_line
+        for ors in line:
+            ans += new_or
+            for num,val in ors:
+                ans += msg_keys['and'] + convert_to_binary_string(num) + str(val)
+        ans += end_line
+
+    ans += end_board
+    return ans
+
+
+c = make_serial([[[(0, 1), (1, 1)]], [[(2, 1), (3, 0)], [(2, 0), (3, 1)]], [[(0, 1), (2, 0)], [(0, 0), (2, 1)]], [[(1, 1), (3, 1)]]])
+i=0
+while i < len(c):
+    print(c[i:i+16])
+    i = i + 16
+    
+    #ser.write(ans.decode('bin'))
 
 def get_sols(spots, conds):
     all_solutions = []
@@ -50,7 +102,12 @@ def make_DNF(rows, cols):
     """
     DNF_rows = generate(rows, cols, False)
     DNF_cols = generate(cols, rows, True)
-    return [DNF_rows,DNF_cols] 
+    formula = make_serial(DNF_rows + DNF_cols)
+    i=0
+    while i < len(formula):
+        print(formula[i:i+16])
+        i = i + 16
+    return DNF_rows + DNF_cols
 
         
 def format_converter(option, line_number, num_ortho_lines,cols=False):
@@ -86,32 +143,16 @@ def generate(lines, lines_ortho, cols_bool=False):
         for option in solutions:
             line_or.append(format_converter(option, i,len(lines), cols_bool))
         DNF_formula.append(line_or)
+    
     return DNF_formula
     
 
 b = [[2],[1]]
 c = [[1],[2]]
-print(make_DNF(b,c))
-    
-  
-# print(format_converter([0,1],1))
 
+        
 
+        
 
+# make_serial([[[[(0, 1), (1, 1)]], [[(2, 1), (3, 0)], [(2, 0), (3, 1)]]], [[[(0, 1), (2, 0)], [(0, 0), (2, 1)]], [[(1, 1), (3, 1)]]]])
 
-# print(format_converter_cols([0,1],1,3))
-
-port = list(list_ports.comports())
-for p in port:
-   print(p.device)
-
-
-ser = serial.Serial('/dev/cu.usbserial-AQ00J7ZN')
-def make_serial(solutions):
-   for line in solutions:
-      for ors in line:
-          for var,val in ors:
-               ser.write(val)
-               ser.write(struct.pack('<H', var))
-          ser.write('\n')
-      ser.write('\n')
