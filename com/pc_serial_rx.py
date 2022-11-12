f0f47a0877952d4f4c6946b6a2d0db669797a093
VeronicaG314 <72225753+VeronicaG314@users.noreply.github.com>
2022-11-12 14:07:23 -0500

    Update pc_serial_rx.py

--- com/pc_serial_rx.py
+++ com/pc_serial_rx.py
@@ -1,6 +1,7 @@
 '''Automatically find USB Serial Port (jodalyst 8/2019)
 '''
 import serial.tools.list_ports
+import time
 
 def get_usb_port():
     usb_port = list(serial.tools.list_ports.grep("USB"))
@@ -24,8 +25,36 @@ def get_usb_port():
             print("USB-Serial Controller: Device {}".format(p))
             return port_dict[usb_id][0].device
 
+def test_rx(ser):
+    try:
+        print("Reading...")
+        while True:
+            data = ser.read(1) #read the buffer (99/100 timeout will hit)
+            if data != b'':  #if not nothing there.
+                if data[0]<=127: #if going to be valid ascii...
+                    print("ASCII: {}, Value: {}".format(data.decode('ascii'),data[0]))
+                else:
+                    print("Invalid ASCII, Value: {}".format(data[0]))
+    except Exception as e:
+        print(e)
+        ser.close()
+
+def test_tx(ser):
+    try:
+        print("Writing...")
+        data = 0
+        while True:
+            ser.write((data).to_bytes(2,'big')) #write the buffer (99/100 timeout will hit)
+            print(str(data) + ", " + str(((data).to_bytes(2,'big'))))
+            data = (data + 1)%100
+            time.sleep(.25)
+    except Exception as e:
+        print(e)
+        ser.close()
+
 s = get_usb_port()  #grab a port
 print("USB Port: "+str(s)) #print it if you got
+
 if s:
     ser = serial.Serial(port = s,
         baudrate=9600,
@@ -40,15 +69,5 @@ else:
     print("No Serial Device :/ Check USB cable connections/device!")
     exit()
 
-try:
-    print("Reading...")
-    while True:
-        data = ser.read(1) #read the buffer (99/100 timeout will hit)
-        if data != b'':  #if not nothing there.
-            if data[0]<=127: #if going to be valid ascii...
-                print("ASCII: {}, Value: {}".format(data.decode('ascii'),data[0]))
-            else:
-                print("Invalid ASCII, Value: {}".format(data[0]))
-except Exception as e:
-    print(e)
-    ser.close()
+#test_rx(ser)
+test_tx(ser)
