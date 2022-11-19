9fbfdc645d2cc8d98e71804e0145ad5d08c708fb
Nina Gerszberg <ninager@mit.edu>
2022-11-19 17:34:57 -0500

    added FIFO sudo code

--- src/FIFO_solver.sv
+++ src/FIFO_solver.sv
@@ -11,13 +11,23 @@ module fifo_solver(
         output logic solved,  //signals solver is done
 );
 
+logic contradict;
+
 if(rst)begin
 
 end else if (len(FIFO) == 0) begin //TODO: Fix this
     solved <= 1;
+    //done return info
 end else begin
+    //pop off top line of queue
 
+    //check if it can be simplified using simplify.sv
 
+    //if it contradicts, remove this option from the queue line
+
+    //if it everything is known and it doesnt contradict, dont add the queue line back to the queue
+
+    //add the queue line back the qeueue
 end
 
 endmodule
