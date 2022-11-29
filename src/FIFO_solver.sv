1f536ff56d5a006cb2696b3b4508bc3fd1f13fee
Nina Gerszberg <ninager@mit.edu>
2022-11-29 17:46:16 -0500

    started to modify solver

--- src/FIFO_solver.sv
+++ src/FIFO_solver.sv
@@ -10,29 +10,39 @@ module fifo_solver (
         input wire clk,
         input wire rst,
         input wire  [SIZE-1:0] option,
-        input wire  [SIZE-1:0] line_ind,
+        
         input wire valid_op,
-        input wire row, //is it a row or a column (line indices repeat, once for the row index and once for the column)
-        input wire  [SIZE:0] option_num,//Taken from the BRAM in the top level- how many options for this line
+        input wire [2*SIZE:0] [6:0] options_amnt,//Taken from the BRAM in the top level- how many options for this line
 
         output logic  [SIZE-1:0]  [SIZE-1:0] assigned,  
         output logic put_back_to_FIFO,  //boolean- do we need to push to fifo
         output logic new_option_num, // for the BRAM gonna either be same as option num or 1 less
         output logic valid_out
     );
+
+        logic [SIZE-1] line_ind; //TODO:What size should this be?
+        assign row = line_ind < SIZE;
         parameter SIZE = 3;
         logic  [SIZE-1:0] [SIZE-1:0] known;
         
+        logic checking_option;
+
+        logic [6:0] options_left; //options left to get from the fifo
+        logic [6:0] net_valid_opts; //how many valid options we checked
+
         logic contradict; //if 1 contradicts and we remove it
         logic simp_valid; //out put valid for simplify
 
         logic  [SIZE-1:0] assi_simp; //one line input of assigned input to simplify
         logic [SIZE-1:0] known_simp; //one line input of known input to simplif
 
+        logic [SIZE-1:0] always1;// a and b
+        logic [SIZE-1:0] always0;
+
         simplify #( (SIZE))simplify_m(
         .clk(clk),
         .rst(rst),
-        .valid_in(valid_op),
+        .valid_in(checking_option),
         .assigned(assi_simp), // SIZE-1:0]
         .known(known_simp),
         .option(option),
@@ -58,19 +68,21 @@ module fifo_solver (
 
 //Grab the line from relevant known and assigned blocks
     always_comb begin
-        if (row) begin
-            assi_simp = assigned[SIZE - line_ind ];
-            known_simp = known[SIZE - line_ind ];
+        if (options_left > 0) begin
+            checking_option = 1;
+            if (row) begin
+                assi_simp = assigned[SIZE - line_ind ];
+                known_simp = known[SIZE - line_ind ];
 
-        end else begin
-            assi_simp = assigned_t[SIZE - line_ind ];
-            known_simp = known_t[SIZE - line_ind ];
-            //for (int i = 0; i < SIZE; i = i + 1)begin
-            //    assi_simp[i] = assigned[i][line_ind];
-            //    known_simp[i] = known[i][line_ind];
-            //end
+            end else begin
+                assi_simp = assigned_t[SIZE - line_ind ];
+                known_simp = known_t[SIZE - line_ind ];
+            end
         end
-
+        else begin 
+            //this is the case where the input to the queue is a line index
+            checking_option = 0;
+        end
     end
     
     always_ff @(posedge clk)begin
@@ -78,33 +90,34 @@ module fifo_solver (
             known <= 0;
             assigned <= 0;
             valid_out <=0;
-        end 
-        
-        else if (option_num == 1 && valid_op) begin
-            //this is the only valid option for the line
-            put_back_to_FIFO <= 0;
-            if (row) begin
-                known[line_ind] <= -1; //-1;//this might be wroing '{1}, suppose to be a whole ist of 1
-                assigned[line_ind] <= option;
-            end else begin
-                //known_t[line_ind] <= 0; //-1;//this might be wroing '{1}
-               // assigned_t[line_ind] <= option;
-               for(integer row = 0; row < SIZE; row = row + 1) begin
-                    known[row*SIZE + line_ind] <= 1;
-                    assigned[row*SIZE + line_ind] <= option[row];
-               end
-            
-            end
-            valid_out<=1;
-        end else if (simp_valid) begin       
-            if (contradict)begin
-                put_back_to_FIFO <= 0;
-                new_option_num <= option_num - 1;
+        end else begin (options_left > 0)begin
+            if (simp_valid) begin       
+                if (contradict)begin
+                    put_back_to_FIFO <= 0;
+                    new_option_num <= option_num - 1;
 
-            end else begin
-                put_back_to_FIFO <= 1;
+                end else begin
+                    put_back_to_FIFO <= 1;
+                end
+                valid_out<=1;
             end
-            valid_out<=1;
+        end else begin
+            //transition to new line 
+            options_left <= options_amnt[line_ind];
+            net_valid_opts <= 0;
+            if (options_amnt[line_ind] == 1) begin
+                //only one valid option
+                put_back_to_FIFO <= 0;
+                if (row) begin
+                    known[line_ind] <= -1; //-1;//this might be wroing '{1}, suppose to be a whole ist of 1
+                    assigned[line_ind] <= option;
+                end else begin
+                    for(integer row = 0; row < SIZE; row = row + 1) begin
+                            known[row*SIZE + line_ind] <= 1;
+                            assigned[row*SIZE + line_ind] <= option[row];
+                    end
+                end
+            end
         end
     end
 
