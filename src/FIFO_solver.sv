1766e3cc9887f8c16d3be7845ac85b259a700f49
Nina Gerszberg <ninager@mit.edu>
2022-11-29 22:30:10 -0500

    fixed one case bug and starter code logic

--- src/FIFO_solver.sv
+++ src/FIFO_solver.sv
@@ -7,6 +7,7 @@
 //when we call array[0] it will give the last 3 bits
 
 module fifo_solver (
+        //TODO: confirm sizes for everything
         input wire clk,
         input wire rst,
         input wire  [SIZE-1:0] option,
@@ -20,7 +21,7 @@ module fifo_solver (
         output logic valid_out
     );
 
-        logic [$clog2(SIZE)] line_ind; //TODO:What size should this be?
+        logic [$clog2(SIZE)] line_ind;
         assign row = line_ind < SIZE;
         parameter SIZE = 3;
         logic  [SIZE-1:0] [SIZE-1:0] known;
@@ -30,10 +31,12 @@ module fifo_solver (
         logic [6:0] options_left; //options left to get from the fifo
         logic [6:0] net_valid_opts; //how many valid options we checked
 
-        logic [SIZE-1:0] last_valid_option; //that is the last valid option we got for this line. we need it for when we transition we want to use it to assign
+        //logic [SIZE-1:0] last_valid_option; //that is the last valid option we got for this line. we need it for when we transition we want to use it to assign
 
         logic contradict; //if 1 contradicts and we remove it
         logic simp_valid; //out put valid for simplify
+
+        logic one_option_case;
 
         logic  [SIZE-1:0] assi_simp; //one line input of assigned input to simplify
         logic [SIZE-1:0] known_simp; //one line input of known input to simplif
@@ -97,24 +100,26 @@ module fifo_solver (
             net_valid_opts <=0;
             last_valid_option<=0;
             started <=0;
-
-        //first first round:
-        end else if (started == 0 && valid_op) begin
-            started <= 1;
-            line_ind <= option;
-            options_left <= options_amnt[0];
-            net_valid_opts <= 0;
-            always1 <= ~0;
-            always0 <= ~0 ;
-
+            one_option_case <= 0;
+        end else if (one_option_case && simp_valid) begin
+            put_back_to_FIFO <= 0;
+            if (row) begin
+                known[line_ind] <= -1; //-1;//this might be wroing '{1}, suppose to be a whole ist of 1
+                assigned[line_ind] <= option;
+            end else begin
+                for(integer row = 0; row < SIZE; row = row + 1) begin
+                    known[row*SIZE + line_ind] <= 1;
+                    assigned[row*SIZE + line_ind] <= option[row];
+                end
+            end
+         
         //if we have options left to check:
-        end else if (options_left > 0)begin
+        end else if (options_left > 0 && simp_valid == 1)begin
             if (simp_valid) begin       
                 if (contradict)begin
                     put_back_to_FIFO <= 0;
-                    // net_valid_opts <= net_valid_opts - 1;
                 end else begin
-                    last_valid_option <= option;
+                    //last_valid_option <= option;
                     put_back_to_FIFO <= 1;
                     net_valid_opts <= net_valid_opts + 1;
                     always1 <= always1 && option;
@@ -125,31 +130,58 @@ module fifo_solver (
             end
 
         //transition to new line, reset some registers
-        end else if (options_left == 0) begin
-            line_ind <= option;
-            options_left <= options_amnt[option];
-            net_valid_opts <= 0;
-            always1 <= ~0;
-            always0 <= ~0 ;
-            //TODO check if specific bits of always1 or always0 are 1, if so assign it to known and assigned accordingly
-            if  () begin
-                
-            end
-
-
-            if (net_valid_opts == 1) begin
-                //only one valid option
-                put_back_to_FIFO <= 0;
-                if (row) begin
-                    known[line_ind] <= -1; //-1;//this might be wroing '{1}, suppose to be a whole ist of 1
-                    assigned[line_ind] <= last_valid_option;
+        //first line logic is now also here (avoids repetitive code)
+        end else if (options_left == 0 || started == 0) begin
+            //TODO: maybe break this into 2 clock cycles, one where we process old values and one where we assign the new ones
+            //unsure if thats nessessary
+            
+            //@DANA - what do you think of combining the starter code logic with this code?
+            if (started == 1) begin
+                options_amnt[line_ind] <= net_valid_opts;
+                options_left <= options_amnt[option];
+                if (options_amnt[option] == 1)begin
+                    one_option_case <= 1;
                 end else begin
-                    for(integer row = 0; row < SIZE; row = row + 1) begin
-                        known[row*SIZE + line_ind] <= 1;
-                        assigned[row*SIZE + line_ind] <= last_valid_option[row];
-                    end
+                    one_option_case <= 0;
+                end
+            end else begin
+                //first line logic
+                started <= 1;
+                options_left <= options_amnt[0];
+                if (options_amnt[0] == 1)begin
+                    one_option_case <= 1;
+                end else begin
+                    one_option_case <= 0;
                 end
             end
+            line_ind <= option;
+            net_valid_opts <= 0;
+            always1 <= -1;
+            always0 <= -1;
+            //TODO check if specific bits of always1 or always0 are 1, if so assign it to known and assigned accordingly
+            if  (row) begin
+                //right now my only idea is to have a for loop and go through the options and check 
+                //but that feels jank so maybe lets ask in office hours first?
+            end else begin
+
+            end
+            
+            
+            
+            //@DANA- I moved this code up, look at the logic for one_option_case
+            //if (net_valid_opts == 1) begin
+                //only one valid option
+                // put_back_to_FIFO <= 0;
+                // if (row) begin
+                //     known[line_ind] <= -1; //-1;//this might be wroing '{1}, suppose to be a whole ist of 1
+                //     assigned[line_ind] <= last_valid_option;
+                // end else begin
+                //     for(integer row = 0; row < SIZE; row = row + 1) begin
+                //         known[row*SIZE + line_ind] <= 1;
+                //         assigned[row*SIZE + line_ind] <= last_valid_option[row];
+                //     end
+                // end
+            //end
         end
     end
 
