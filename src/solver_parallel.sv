2acdc4b31ec43bb6551a72585a539f4f7c9b9f5e
danaru29 <64731754+danaru29@users.noreply.github.com>
2022-12-14 12:57:45 -0500

    attempted to parallel. doesn't solve correctly :/

--- src/solver_parallel.sv
+++ src/solver_parallel.sv
@@ -7,7 +7,7 @@
 //I figured maybe this would just be an input constraint that this doesn't happen but now I'm unclear given 
 // we also had this for the non-parallel version but there is an unsolvable testbench. 
 //Would you prefer we check for this?
-module parrallel_solver #(parameter MAX_ROWS = 11, parameter MAX_COLS = 11, parameter MAX_NUM_OPTIONS=84)(
+module parallel_solver #(parameter MAX_ROWS = 11, parameter MAX_COLS = 11, parameter MAX_NUM_OPTIONS=84)(
         //TODO: confirm sizes for everything
         input wire clk,
         input wire rst,
@@ -18,6 +18,7 @@ module parrallel_solver #(parameter MAX_ROWS = 11, parameter MAX_COLS = 11, para
         input wire [$clog2(MAX_COLS) - 1:0] num_cols,
         //@Nina, @Veronica -We may have to divide this:
         input wire [MAX_ROWS + MAX_COLS - 1:0] [$clog2(MAX_NUM_OPTIONS)-1:0] old_options_amnt,  //[0:2*SIZE] [6:0]
+        input wire [MAX_ROWS + MAX_COLS - 1:0] [$clog2(MAX_NUM_OPTIONS)-1:0] old_options_amnt_c,
         //TODOI:  unsure if there would be issues with accessing it twice witihn the same clock cycle
             //idk if you'd like one for rows and one for columns seperately
 
@@ -43,6 +44,7 @@ module parrallel_solver #(parameter MAX_ROWS = 11, parameter MAX_COLS = 11, para
 
     localparam LARGEST_DIM = (MAX_ROWS > MAX_COLS)? MAX_ROWS : MAX_COLS;
     logic [MAX_ROWS + MAX_COLS - 1:0] [6:0] options_amnt;// options_amnt2; 
+    logic [MAX_ROWS + MAX_COLS - 1:0] [6:0] options_amnt_c;// options_amnt2; 
     
     logic [2:0][$clog2(MAX_ROWS + MAX_COLS) - 1:0] line_index1, new_index1;
     logic [2:0][$clog2(MAX_ROWS + MAX_COLS) - 1:0] line_index2, new_index2;
@@ -77,6 +79,7 @@ module parrallel_solver #(parameter MAX_ROWS = 11, parameter MAX_COLS = 11, para
 
     logic  [(MAX_ROWS * MAX_COLS) - 1:0] known_t; //transpose
     logic  [(MAX_ROWS * MAX_COLS) - 1:0] assigned_t; //transpose
+    // logic assign_r, assign_c; //when 1, assign row and then the col, potentially just assign row and then assign col
 
     //TRANSPOSING:
     genvar m; //rows
@@ -122,17 +125,18 @@ module parrallel_solver #(parameter MAX_ROWS = 11, parameter MAX_COLS = 11, para
             state2 <= IDLE;
             first1 <= 1;
             first2 <= 1;
-            new_index1 <= '0;
-            new_index2 <= '0;
-            options_amnt <= '0;
-            line_index1 <= '0;
-            line_index2 <= '0;
-            base_index1<= '0;
-            base_index2<= '0;
-            read_from_fifo_r<= '0;
-            read_from_fifo_c<= '0;
-            new_option_r<= '0;
-            new_option_c<= '0;
+            new_index1 <= 0;
+            new_index2 <= 0;
+            options_amnt <= 0;
+            options_amnt_c<=0;
+            line_index1 <= 0;
+            line_index2 <= 0;
+            base_index1<= 0;
+            base_index2<= 0;
+            read_from_fifo_r<= 0;
+            read_from_fifo_c<= 0;
+            new_option_r<= 0;
+            new_option_c<= 0;
             put_back_to_FIFO_r<=0;
             put_back_to_FIFO_c<=0;    
         end else 
@@ -224,6 +228,7 @@ module parrallel_solver #(parameter MAX_ROWS = 11, parameter MAX_COLS = 11, para
                 IDLE: begin
                     if (started)begin
                         read_from_fifo_c <= 1;
+                        options_amnt_c <= old_options_amnt_c;
                         state2 <= NEXT_LINE_INDEX;
                     end
                     first2 <= 1;
@@ -231,16 +236,16 @@ module parrallel_solver #(parameter MAX_ROWS = 11, parameter MAX_COLS = 11, para
                 NEXT_LINE_INDEX: begin
                     if (num_known_cols == num_cols)begin
                         //victory check
-                        solved <= 1;
+                        // solved <= 1;
                         state2 <= IDLE;
                         read_from_fifo_c <= 0;
                         put_back_to_FIFO_c <= 0;
                     end else begin
-                        if(!first2) options_amnt[line_index2[2]] <= net_valid_opts2;
-                            if (options_amnt[option_c] > 0) begin
-                            state2 <= (options_amnt[option_c]==0)? NEXT_LINE_INDEX : (options_amnt[option_c] == 1)? ONE_OPTION : MULTIPLE_OPTIONS;
+                        if(!first2) options_amnt_c[line_index2[2]] <= net_valid_opts2;
+                            if (options_amnt_c[option_c] > 0) begin
+                            state2 <= (options_amnt_c[option_c]==0)? NEXT_LINE_INDEX : (options_amnt_c[option_c] == 1)? ONE_OPTION : MULTIPLE_OPTIONS;
                         end
-                        options_left2 <= options_amnt[option_c];
+                        options_left2 <= options_amnt_c[option_c];
                         new_index2 <= option_c;
                         line_index2[0] <= option_c;
                         put_back_to_FIFO_c <= 1;
@@ -279,24 +284,31 @@ module parrallel_solver #(parameter MAX_ROWS = 11, parameter MAX_COLS = 11, para
                     read_from_fifo_c <= 0;
                 end
                 WRITE: begin
-                    read_from_fifo_c <= 1;
-                    // check if specific bits of always1 or always0 are 1, if so assign it to known and assigned accordingly
-                    base_index2 = (line_index2[1] - num_rows);
-                    for(integer j = 0; j < MAX_ROWS; j = j + 1) begin
-                        if(j < num_rows) begin
-                            if (always1_2[j]) begin 
-                                known[base_index2] <= 1; 
-                                assigned[base_index2] <= 1;
+                    if (state1==WRITE) begin
+                        state2 <= WRITE;
+                        read_from_fifo_c <= 0;
+                        put_back_to_FIFO_c <= 0;
+                    end else begin
+                        state2 <= NEXT_LINE_INDEX;
+                        read_from_fifo_c <= 1;
+                        // check if specific bits of always1 or always0 are 1, if so assign it to known and assigned accordingly
+                        base_index2 = (line_index2[1] - num_rows);
+                        for(integer j = 0; j < MAX_ROWS; j = j + 1) begin
+                            if(j < num_rows) begin
+                                if (always1_2[j]) begin 
+                                    known[base_index2] <= 1; 
+                                    assigned[base_index2] <= 1;
+                                end
+                                if (always0_2[j]) begin 
+                                    known[base_index2] <= 1; 
+                                    assigned[base_index2] <= 0;
+                                end
                             end
-                            if (always0_2[j]) begin 
-                                known[base_index2] <= 1; 
-                                assigned[base_index2] <= 0;
-                            end
+                            base_index2 += MAX_COLS;
                         end
-                        base_index2 += MAX_COLS;
+                        state2 <= NEXT_LINE_INDEX;
+                        put_back_to_FIFO_c <= 0;
                     end
-                    state2 <= NEXT_LINE_INDEX;
-                    put_back_to_FIFO_c <= 0;
                 end
             endcase
         //end
